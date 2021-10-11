d87e9cdc3a72f74581bcfc0b5152a4ed570fb930
greenjoe071 <84485320+greenjoe071@users.noreply.github.com>
2021-10-11 17:28:51 -0500

    added deploy.js  again

--- deploy.js
+++ deploy.js
@@ -85,7 +85,7 @@ function sshConnect() {
   ssh
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: '172.31.88.140',
+      host: '54.167.238.218',
       username: 'ubuntu',
       privateKey: 'final.pem'
     })
